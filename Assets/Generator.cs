c5ea4f7df0035844b1e86de44d6b4f638d36d51f
GT20176040\user <nkc.iori.teshima@gmail.com>
2017-10-26 10:57:05 +0900

    k

---
+++ Assets/Generator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public GameObject Prefab;
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject tame =
+                Instantiate(Prefab) as GameObject;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            Prefab.GetComponent<Controller>().Shoot(
+             worldDir.normalized * 2000);
+        }
+	}
+}
