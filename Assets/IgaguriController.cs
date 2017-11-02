53a4b70b1f49af12dddec8fa6cf7b805f84a0fb4
GT20176040\user <nkc.iori.teshima@gmail.com>
2017-11-02 09:50:13 +0900

    リンゴと爆弾仮設置

---
+++ Assets/IgaguriController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgaguriController : MonoBehaviour {
+
+
+    GameObject target;
+    public void Shoot(Vector3 dir) {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    
+    void OnCollisionEnter(Collision other)
+    {
+        //GetComponent<Rigidbody>().isKinematic = true;
+        //GetComponent<ParticleSystem>().Play();
+        this.target = GameObject.Find("target");
+        Destroy(target);
+        this.target = GameObject.Find("target2");
+        Destroy(target);
+    }
+
+
+    void Start () {
+       
+        Shoot(new Vector3(0, 200, 2000));
+    }
+}
---
+++ Assets/IgaguriGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgaguriGenerator : MonoBehaviour
+{
+
+    public GameObject igaguriPrefab;
+
+    void Update()
+    {            igaguri.GetComponent<IgaguriController>().Shoot(
+        if (Input.GetMouseButtonDown(0)){
+            GameObject igaguri =
+                Instantiate(igaguriPrefab) as GameObject;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+
+                worldDir.normalized * 2000);
+        }
+    }
+}
